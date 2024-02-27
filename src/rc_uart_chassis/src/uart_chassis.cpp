c59bee5e0d6ea73b5b28d5bf13d3f4058774abd7
Davin-Liang <1528994924@qq.com>
2024-02-27 09:47:40 +0800

    修改了 uart_chassis.cpp

--- src/rc_uart_chassis/src/uart_chassis.cpp
+++ src/rc_uart_chassis/src/uart_chassis.cpp
@@ -18,7 +18,21 @@ private:
 
     void FrameCallback(const std_msgs::msg::String::SharedPtr msg)
     {
-        
+    	switch (msg.data)
+	{
+		case "1":
+			break;
+		case "2":
+                        break;
+		case "3":
+                        break;
+		case "4":
+                        break;
+		case "5":
+                        break;
+		case "y":
+                        break;
+	}	
     }
 };
 
