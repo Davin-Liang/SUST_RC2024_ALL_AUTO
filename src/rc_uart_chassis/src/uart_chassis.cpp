049e4befcf4a04c57b5957ec56da4c8caa6bc574
Davin-Liang <1528994924@qq.com>
2024-02-27 09:19:53 +0800

    修改了 uart_chassis

--- src/rc_uart_chassis/src/uart_chassis.cpp
+++ src/rc_uart_chassis/src/uart_chassis.cpp
@@ -1,4 +1,5 @@
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 
 class UartCommander : public rclcpp::Node
 {
@@ -7,10 +8,18 @@ public:
     UartCommander(std::string name) : Node(name)
     {
         RCLCPP_INFO(this->get_logger(), "%s节点已经启动.", name.c_str());
+	FrameSubscribe_ = this->create_subscription<std_msgs::msg::String>("OptimalFrame", 10, std::bind(&UartCommander::FrameCallback, this, std::placeholders::_1));
     }
 
 private:
     // 声明节点
+    // 声明一个订阅者
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr FrameSubscribe_;
+
+    void FrameCallback(const std_msgs::msg::String::SharedPtr msg)
+    {
+        
+    }
 };
 
 int main(int argc, char **argv)
