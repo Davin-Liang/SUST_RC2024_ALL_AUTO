039b68023dc17c794a57a9e5efd12bce8d6b1c3f
Davin-Liang <1528994924@qq.com>
2024-02-28 11:30:06 +0800

    在 uart_chassis.cpp 中增加了初始化串口的代码

--- src/rc_uart_chassis/src/uart_chassis.cpp
+++ src/rc_uart_chassis/src/uart_chassis.cpp
@@ -1,5 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include <serial/serial.h>
 
 class UartCommander : public rclcpp::Node
 {
@@ -8,15 +9,46 @@ public:
     UartCommander(std::string name) : Node(name)
     {
         RCLCPP_INFO(this->get_logger(), "%s节点已经启动.", name.c_str());
-	FrameSubscribe_ = this->create_subscription<std_msgs::msg::String>("OptimalFrame", 10, std::bind(&UartCommander::FrameCallback, this, std::placeholders::_1));
+	    FrameSubscribe_ = this->create_subscription<std_msgs::msg::String>("OptimalFrame", 10, std::bind(&UartCommander::FrameCallback, this, std::placeholders::_1));
+
+        /* 从参数服务器中获取参数 */
+        this->declare_parameter<std::string>("dev", "");
+        this->declare_parameter<int>("baud", 115200);
+        this->declare_parameter<int>("time_out", 1000);
+        this->declare_parameter<int>("hz", "100");
+
+        this->get_parameter_or<std::string>("dev", dev, "");
+        this->get_parameter_or<int>("baud", baud, "115200");
+        this->get_parameter_or<int>("time_out", time_out, "1000");
+        this->get_parameter_or<int>("hz", hz, "100");
+
+        /* 开启串口模块 */
+        try
+        {
+            ros_ser.setPort(dev);
+            ros_ser.setBaudrate(baud);
+            
+            serial::Timeout to = serial::Timeout::simpleTimeout(time_out);
+            ros_ser.setTimeout(to);
+            ros_ser.open();
+            ros_ser.flushInput(); // 清空缓冲区数据
+        }
+        catch (serial::IOException& e)
+        {
+            ROS_ERROR_STREAM("Unable to open port.");
+        }
     }
 
 private:
     // 声明节点
     // 声明一个订阅者
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr FrameSubscribe_;
+    serial::Serial ros_ser;
+    bool uart_recive_flag;
+    std::string dev; // 串口号
+    int baud, time_out, hz; // 波特率，延时时间，发布频率
 
-    void FrameCallback(const std_msgs::msg::String::SharedPtr msg)
+    void FrameCallback( const std_msgs::msg::String::SharedPtr msg )
     {
     	switch (msg.data)
 	{
